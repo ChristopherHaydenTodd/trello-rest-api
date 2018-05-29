e0455f58c4fbd8382ec7e98c03a4bc2ecd313846
Christopher Todd <Christopher_Todd@cable.comcast.com>
2018-05-29 15:02:49 -0400

    updating Trello class with all methods required for getting any board, card, and list

--- classes/trello.py
+++ classes/trello.py
@@ -42,18 +42,47 @@ class Trello(object):
         self.trello_api.set_token(CONFIGS.TRELLO_APP_TOKEN)
 
         self.known_boards = {
-            'To Do List': '56e1cbce8bde063d47d1db02',
+            '56e1cbce8bde063d47d1db02': 'To Do List',
         }
 
         self.board = None
         self.lists = None
         self.cards = None
 
+    def convert_board_id_to_name(self, board_id):
+        """
+        Purpose:
+            Return Board Name Based on Board ID
+        Args:
+            board_id (string): Unique ID for Board
+        Return
+            board_name (string): Non-unique name for board
+        """
+
+        return self.known_boards.get(board_id, None)
+
+    def convert_board_name_to_id(self, board_name):
+        """
+        Purpose:
+            Return Board ID Based on Board Name
+        Args:
+            board_name (string): Non-unique name for board
+        Return
+            board_id (string): Unique ID for Board
+        """
+
+        board_id = None
+        for loop_board_id, loop_board_name in\
+            self.known_boards.items():
+            if board_name == loop_board_name:
+                return board_id
+
+        return board_id
 
     def get_boards(self):
         """
         Purpose:
-            Return Known Boards
+            Return All Boards Available to Pull
         Args:
             N/A
         Return
@@ -65,30 +94,89 @@ class Trello(object):
         to get all boards available to a user
         """
 
-        return list(self.known_boards.keys())
+        return self.known_boards
 
-    # def get_board(board_name):
-    #     """
-    #         Get the Board Object from the Trello API
-    #         Based on Passed in Boards
-    #     """
+    def get_board_details(self, board_id):
+        """
+        Purpose:
+            Return All Board Details Based on Board Passed In
+        Args:
+            board_id (string): Unique ID for Board
+        Return
+            board_details (dictionary): Board Object from API
+        """
 
-    # def get_lists(self, board_id):
-    #     """
-    #         Initilize the Base CMTS Class.
-    #         Class Properties Should be set in Child Class
-    #     Args:
-    #         fqdn (string): hostname or fqdn
-    #     """
+        board_details =\
+            self.trello_api.boards.get(board_id)
+        board_details['cards'] =\
+            self.trello_api.boards.get_card(board_id)
+        board_details['lists'] =\
+            self.trello_api.boards.get_list(board_id)
 
-    #     return
+        return board_details
 
-    # def get_lists(self, list_id):
-    #     """
-    #         Initilize the Base CMTS Class.
-    #         Class Properties Should be set in Child Class
-    #     Args:
-    #         fqdn (string): hostname or fqdn
-    #     """
+    def get_lists(self, board_id):
+        """
+        Purpose:
+            Return All Lists in the passed in Board
+        Args:
+            board_id (string): Unique ID for Board
+        Return
+            lists (list of string): List of lists boards
+                With basic details
+        """
 
-        return
+        lists = self.trello_api.boards.get_list(board_id)
+
+        return lists
+
+    def get_list_details(self, list_id):
+        """
+        Purpose:
+            Return All List Details Based on List Passed In
+        Args:
+            list_id (string): Unique ID for List
+        Return
+            list_details (dictionary): List Object from API
+        """
+
+        list_details =\
+            self.trello_api.lists.get(list_id)
+        list_details['cards'] =\
+            self.trello_api.lists.get_card(list_id)
+
+        return list_details
+
+    def get_cards(self, board_id=None, list_id=None):
+        """
+        Purpose:
+            Return All Lists in the passed in Board
+        Args:
+            board_id (string): Unique ID for Board
+        Return
+            cards (card of string): List of cards boards
+                With basic details
+        """
+
+        if board_id:
+            cards = self.trello_api.boards.get_card(board_id)
+        elif list_id:
+            cards = self.trello_api.lists.get_card(list_id)
+        else:
+            cards = None
+        return cards
+
+    def get_card_details(self, card_id):
+        """
+        Purpose:
+            Return All List Details Based on List Passed In
+        Args:
+            card_id (string): Unique ID for List
+        Return
+            card_details (dictionary): List Object from API
+        """
+
+        card_details =\
+            self.trello_api.cards.get(card_id)
+
+        return card_details
